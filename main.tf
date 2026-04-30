aacd0c4ee06db4a97abcdbe94b5769dc20b0d8b9
ruchikr2808 <ruchikr2808@gmail.com>
2026-04-30 15:28:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+resource "random_pet" "server_name" {
+  length    = 2
+  separator = "-"
+}
+
+resource "null_resource" "test" {
+  triggers = {
+    name = random_pet.server_name.id
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'Hello from Terraform! Server name: ${random_pet.server_name.id}'"
+  }
+}
