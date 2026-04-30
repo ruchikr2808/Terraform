68e48198218550232f025d9e46ffcc3685c62864
ruchikr2808 <ruchikr2808@gmail.com>
2026-04-30 16:13:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,23 @@
 terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
 
 resource "random_pet" "server_name" {
-  length    = 2
-  separator = "-"
+  length    = 2
+  separator = "-"
 }
 
 resource "null_resource" "ctm_trigger_test" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = "echo 'Triggered by Control-M - Server: ${random_pet.server_name.id}'"
-  }
+  triggers = {
+    always_run = timestamp()
+  }
 }
